a3475766f16edf578c75e99623d5e862df9e5783
djunior92 <juninho_2509@hotmail.com>
2021-04-29 11:50:02 -0300

    Created .travis.yml file

---
+++ src/main/java/com/djalmajunior/bookstoremanager/config/SwaggerConfig.java
@@ -0,0 +1,46 @@
+package com.djalmajunior.bookstoremanager.config;
+
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import springfox.documentation.builders.ApiInfoBuilder;
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.service.Contact;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.service.ApiInfo;
+
+@Configuration
+@EnableSwagger2
+public class SwaggerConfig {
+
+    private static final String BASE_PACKAGE = "com.djalmajunior.bookstoremanager";
+    private static final String API_TITLE = "Bookstore Manager";
+    private static final String API_DESCRIPTION = "Bookstore Manager API Professional";
+    private static final String API_VERSION = "1.0.0";
+    private static final String CONTACT_NAME = "Djalma Junior";
+    private static final String CONTACT_GITHUB = "http:github.com/djunior92";
+    private static final String CONTACT_EMAIL = "juninho_2509@hotmail.com";
+
+    @Bean
+    public Docket api(){
+        return new Docket(DocumentationType.SWAGGER_2)
+                .select()
+                .apis(RequestHandlerSelectors.basePackage(BASE_PACKAGE))
+                .paths(PathSelectors.any())
+                .build()
+                .apiInfo(buildApiInfo());
+    }
+
+    private ApiInfo buildApiInfo() {
+        return new ApiInfoBuilder()
+                .title(API_TITLE)
+                .description(API_DESCRIPTION)
+                .version(API_VERSION)
+                .contact(new Contact(CONTACT_NAME, CONTACT_GITHUB, CONTACT_EMAIL))
+                .build();
+    }
+
+}
